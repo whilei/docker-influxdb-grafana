a5e642628d944539ddf00d9827f79685b3602be5
ia <isaac.ardis@gmail.com>
2021-02-24 07:40:21 -0600

    add my own run script

---
+++ wrun.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+docker build -t docker-influxdb-grafana-img .
+
+docker run -d \
+	--name docker-influxdb-grafana \
+	-p 3003:3003 \
+	-p 3004:8083 \
+	-p 8086:8086 \
+	-v /SAMSUNG_T5_3/influxdb:/var/lib/influxdb \
+	-v /SAMSUNG_T5_3/grafana:/var/lib/grafana \
+	docker-influxdb-grafana-img
